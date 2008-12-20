234383929bb217d30c21a91158593ca54055e652
skibaa <skibaa@34ba4a3c-cbc1-11dd-a8b6-f5aca7b582bb>
2008-12-20 08:25:33 +0000

    use expando for polymorphic one to many relationships

--- smart_sweeper/game/models.py
+++ smart_sweeper/game/models.py
@@ -4,67 +4,98 @@ import logging
 class UserPrefs(db.Model):
     user = db.UserProperty()
 
-def find_subclasses(module, clazz):
-    for name in dir(module):
-        o = getattr(module, name)
-        try:
-            if (o != clazz) and issubclass(o, clazz):
-                yield name, o
-        except TypeError: pass
-
 class BoardType(db.Model):
     name = db.StringProperty()
-
-    def getBoardsByName(name):
-        for clazz in find_subclasses(game.models, BoardType):
-            for res in clazz.gql("WHERE name=:name", name=name):
-                yield res
 
 class RectangleBoardType(BoardType):
     width = db.IntegerProperty()
     height = db.IntegerProperty()
 
-class Game(db.Model):
-    user = db.UserProperty()
-    isComplete = db.BooleanProperty()
-    startDate = db.DateTimeProperty(auto_now_add = 1)
-    boardTypeName = db.StringProperty()
-    
-    def getBoardType(self):
-        if self.key == None:
-            self.put() # so key is known
-        boardTypes = list(BoardType.getBoardByName(boardTypeName))
-        assert len(boardTypes) == 1       
-        return boardTypes[0]
-    
-    def startGame(self):
-        board_id=str(self.getBoardType().key)
-        init_cells = Cell.gql("WHERE boardOrGameId=:boardId", board_id)
-        for cell in init_cells:
-            cell.clone(board_id).put()
+class ReferencesBoardType(object):
+    #property board_type_key = any_board_type.key()
+    #property board_type_kind = any_board_type.kind()
 
-class Cell(db.Model):
-    boardOrGameKey = db.StringProperty() #board id before game, game id later
+    def _get_board_type(self):
+        if (not 'board_type_key' in self.dynamic_properties()) or (not 'board_type_kind' in self.dynamic_properties()):
+            return None
+        kind=globals()[self.board_type_kind]
+        return kind.get(board_type_key)
+
+    def _set_board_type(self, aBoard_type):
+        if not aBoard_type:
+            del self.board_type
+        self.board_type_key = aBoard_type.key()
+        self.board_type_kind = aBoard_type.kind()
+
+    def _delete_board_type(self):
+        del self.board_type_key
+        del self.board_type_kind
+
+    board_type=property(_get_board_type, _set_board_type, _delete_board_type)
+    
+class Game(db.Expando, ReferencesBoardType):
+    user = db.UserProperty()
+    is_complete = db.BooleanProperty()
+    start_date = db.DateTimeProperty(auto_now_add = 1)
+
+    def start_game(self):
+        for cell in Cell.get_by_board(self.board_type):
+            cell_in_game=cell.create_attached_to_game(self)
+            cell_in_game.put()
+
+class Cell(db.Expando, ReferencesBoardType):
     coord = db.StringProperty() #coordinates of me - depends on board type
     isOpen = db.BooleanProperty()
     isBomb = db.BooleanProperty()
-    neighboursCoords = db.StringListProperty()
+    neighbours_coords = db.StringListProperty()
 
-    def init(self, boardOrGameKey, neighbours):
-        self.boardOrGameKey=boardOrGameKey
-        self.isOpen=False
-        self.isBomb=False
+    def init(self, board_type, game, neighbours):
+        self.board_type=board_type
+        if game and game.has_key():
+            self.game = game.key()
+        else:
+            self.game = None
         for neighbour in neighbours:
-            his_neighbours = set(neighbour.neighboursCoords)
-            if not his_neighbours.contains(self.coord):
-                neighbour.neighbours.add(self.coord)
-            self.neighboursCoords.add(neighbour.coord)
+            if not self.coord in neighbour.neighbours_coords:
+                neighbour.neighbours_coords += [self.coord]
+            self.neighbours_coords += [neighbour.coord]
 
-    def clone(self, boardOrGameKey):
-        return Cell(
-            coord=self.coord,
-            boardOrGameId=boardOrGameKey,
-            isOpen=self.isOpen,
-            isBomb=self.isBomb,
-            neighboursCoords=self.neighboursCoords
+    def create_attached_to_game(self, game):
+        new_cell = Cell(coord=self.coord,
+            neighbours_coords=self.neighboursCoords
         )
+        #TODO see if expando properties can be set in constructor
+        new_cell.board_type=self.board_type
+        new_cell.game=game
+
+    @property
+    def neighbours(self):
+        return self.gql("""WHERE :coords in neighbours_coords
+            and board_type_key=:btkey
+            and board_type_kind=:btkind
+            and game=:game""",
+            coords=coord,
+            btkey=self.board_type_key,
+            btkind=self.board_type_kind,
+            game=game)
+
+    @classmethod
+    def get_by_board(cls, board_type):
+        return cls.gql("""WHERE
+            board_type_key=:btkey
+            and board_type_kind=:btkind
+            and game=:game""",
+            btkey=board_type.key(),
+            btkind=board_type.kind(),
+            game=None)
+
+    @classmethod
+    def get_by_board_and_game(cls, board_type, game):
+        return cls.gql("""WHERE
+            board_type_key=:btkey
+            and board_type_kind=:btkind 
+            and game=:game""",
+            btkey=board_type.key(),
+            btkind=board_type.kind(),
+            game=game)
+
