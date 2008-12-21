e920e4c7f98847bb34b9a09b60964933b8531beb
skibaa <skibaa@34ba4a3c-cbc1-11dd-a8b6-f5aca7b582bb>
2008-12-21 23:11:20 +0000

    add/delete board types, rectangle board game template finds cells correctly

--- trunk/game/models.py
+++ trunk/game/models.py
@@ -20,6 +20,17 @@ class RectangleBoardType(BoardType):
     def _str_coord(row, col):
         return "%d %d"%(row, col)
 
+    def get_cells_for_template(self, game):
+        assert game.board == self
+        cells = [[i]*self.width for i in range(self.height)]
+        wasThere=False
+        for cell in game.cell_set.fetch(1000):
+            wasThere = True
+            (row, col)=cell.coord.split()
+            cells[int(row)][int(col)]=cell
+        assert wasThere
+        return cells
+
     def _calc_neighbours_coords(self, r, c):
         result_tuples=[(r-1, c-1),(r-1, c), (r-1, c+1),
             (r, c-1),     (r, c+1),
@@ -38,7 +49,7 @@ class RectangleBoardType(BoardType):
             for c in range(0, self.width):
                 cell = Cell(coord=self._str_coord(r, c),
                     neighbours_coords=self._calc_neighbours_coords(r, c),
-                    boardType=self
+                    board=self
                     )
                 cell.put()
 
@@ -46,39 +57,38 @@ class Game(db.Model):
     user = db.UserProperty()
     is_complete = db.BooleanProperty()
     start_date = db.DateTimeProperty(auto_now_add = 1)
-    board_type = db.ReferenceProperty(BoardType)
+    board = db.ReferenceProperty(BoardType)
 
     def start_game(self):
-        for cell in Cell.get_by_board(self.board_type):
-            cell_in_game=cell.create_attached_to_game(self)
-            cell_in_game.put()
-
+        for cell in self.board.cell_set:
+            cell.create_attached_to_game(self)
+        self.board.put() #so the cell_set is updated
 
 class Cell(db.Model):
     coord = db.StringProperty() #coordinates of me - depends on board type
     isOpen = db.BooleanProperty()
     isBomb = db.BooleanProperty()
     neighbours_coords = db.StringListProperty()
-    boardType = db.ReferenceProperty(BoardType)
+    board = db.ReferenceProperty(BoardType)
     game = db.ReferenceProperty(Game) #prototype Cell if game==None
 
     def create_attached_to_game(self, game):
-        new_cell = Cell(coord=self.coord,
-            neighbours_coords=self.neighboursCoords,
-            board_type=self.board_type,
+        cell = Cell(coord=self.coord,
+            neighbours_coords=self.neighbours_coords,
+            board=self.board,
             game=game
         )
+        cell.put()
+        assert not cell.key()==self.key()
 
     @property
     def neighbours(self):
         return self.gql("""WHERE :coords in neighbours_coords
-            and board_type_key=:btkey
-            and board_type_kind=:btkind
+            and board=:board
             and game=:game
             order by coord
             """,
             coords=coord,
-            btkey=self.board_type_key,
-            btkind=self.board_type_kind,
+            btkey=self.board,
             game=game)
 
--- trunk/urls.py
+++ trunk/urls.py
@@ -4,6 +4,8 @@ urlpatterns = patterns('',
     (r'^$', 'game.views.index'),
     (r'^admin/$', 'game.views.admin'),
     (r'^admin/board/new/$', 'game.views.new_board'),
+    (r'^admin/board/(?P<board_key>\w+)/$', 'game.views.view_board'),
+    (r'^admin/board/(?P<board_key>\w+)/delete/$', 'game.views.del_board'),
     (r'^game/new/$', 'game.views.new_game'),
     (r'game/(\d+)/$', 'game.views.game'),
 )
