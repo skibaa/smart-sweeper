eae6a97e6314d3c6890b7e4d755f79c30c47352b
skibaa <skibaa@34ba4a3c-cbc1-11dd-a8b6-f5aca7b582bb>
2009-01-05 23:07:32 +0000

    start using db.put for performance fix

--- test_delete.py
+++ test_delete.py
@@ -7,15 +7,8 @@ print ''
 total_t=time()
 
 class C(db.Model):
- i=db.IntegerProperty()
+ pass
  
-for i in range(10):
-    t=time()
-    count=0
-    for c in C.all():
-        c.delete()
-        count+=1
-    if not count: break
-    print "deleted", count, time()-t
+db.delete(C.all())
 
 print "total time:", time()-total_t
