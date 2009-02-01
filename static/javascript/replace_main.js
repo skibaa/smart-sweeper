981eab26dfecbbad839512c6304a3605f390fedf
skibaa <skibaa@34ba4a3c-cbc1-11dd-a8b6-f5aca7b582bb>
2009-02-01 22:28:24 +0000

    start ajax without waiting for effect finishing

--- static/javascript/replace_main.js
+++ static/javascript/replace_main.js
@@ -3,22 +3,34 @@ hl_replace_main = function (element, url) {
     return replace_main (url);
 }
 replace_main = function (url) {
+    var hideEffectComplete=false;
+    var ajaxResult=null;
     new Effect.SlideUp('main_div', {
-      duration: .3,
-      afterFinish: function () {
-        new Ajax.Request(url,
-          {
-            method:'get',
-            onSuccess: function(transport){
-              $('main_div').innerHTML=transport.responseText;
-              new Effect.SlideDown('main_div');
-            },
-            onFailure: function(transport){
-              $('main_div').innerHTML='<div>request to '+transport.request.url+' resulted in status '+transport.status+'</div>';
-              new Effect.SlideDown('main_div');
+        afterFinish: function () {
+            complete=true;
+            if (ajaxResult != null) {
+                $('main_div').innerHTML=ajaxResult;
+                new Effect.SlideDown('main_div', {duration: .3});
             }
-          })
-      }
+        }
+    });
+    new Ajax.Request(url,
+    {
+        method:'get',
+        onSuccess: function(transport){
+          ajaxResult=transport.responseText;
+          if (hideEffectComplete) {
+             $('main_div').innerHTML=ajaxResult;
+             new Effect.SlideDown('main_div', {duration: .3});
+          }
+        },
+        onFailure: function(transport){
+          ajaxResult='<div>request to '+transport.request.url+' resulted in status '+transport.status+'</div>';
+          if (hideEffectComplete) {
+             $('main_div').innerHTML=ajaxResult;
+             new Effect.SlideDown('main_div', {duration: .3, queue:'end'});
+          }
+        }
     });
     return false;
 }
